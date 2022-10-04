3b02eca32c65b0cb2641083a5bedbc2408ae905b
liuye <csu1704liuye@163.com>
2022-10-04 20:14:00 +0800

    Update

--- CycleGAN-liuye-master/Crop_Image.py
+++ CycleGAN-liuye-master/Crop_Image.py
@@ -31,3 +31,24 @@ iter_121800_gan_image = iter_121800_image[:227, 227:454]
 iter_121800_gan_image = cv2.resize(iter_121800_gan_image, (224, 224))
 cv2.imwrite('M:/CycleGAN(WSSS)/File/image/iter_120000_gan_image.png', iter_121800_gan_image)
 
+iter_6600_image = cv2.imread(r'M:\CycleGAN(WSSS)\File\image\iter-000006600.jpg')
+iter_6600_crack_image = iter_6600_image[:227, :227]
+iter_6600_crack_image = cv2.resize(iter_6600_crack_image, (224, 224))
+cv2.imwrite('M:/CycleGAN(WSSS)/File/image/iter_6600_crack_image.png', iter_6600_crack_image)
+iter_6600_gan_image = iter_6600_image[:227, 227:454]
+iter_6600_gan_image = cv2.resize(iter_6600_gan_image, (224, 224))
+cv2.imwrite('M:/CycleGAN(WSSS)/File/image/iter_6600_gan_image.png', iter_6600_gan_image)
+iter_6600_att_image = iter_6600_image[:227, 454:454+227]
+iter_6600_att_image = cv2.resize(iter_6600_att_image, (224, 224))
+cv2.imwrite('M:/CycleGAN(WSSS)/File/image/iter_6600_att_image.png', iter_6600_att_image)
+
+iter_26800_image = cv2.imread(r'M:\CycleGAN(WSSS)\File\image\iter-000026800.jpg')
+iter_26800_crack_image = iter_26800_image[:227, :227]
+iter_26800_crack_image = cv2.resize(iter_26800_crack_image, (224, 224))
+cv2.imwrite('M:/CycleGAN(WSSS)/File/image/iter_26800_crack_image.png', iter_26800_crack_image)
+iter_26800_gan_image = iter_26800_image[:227, 227:454]
+iter_26800_gan_image = cv2.resize(iter_26800_gan_image, (224, 224))
+cv2.imwrite('M:/CycleGAN(WSSS)/File/image/iter_26800_gan_image.png', iter_26800_gan_image)
+iter_26800_att_image = iter_26800_image[:227, 454:454+227]
+iter_26800_att_image = cv2.resize(iter_26800_att_image, (224, 224))
+cv2.imwrite('M:/CycleGAN(WSSS)/File/image/iter_26800_att_image.png', iter_26800_att_image)
